c22e4ef42b8ec0830aaafcf9b194fdbcc70dc848
Christopher Forti <christopher.forti.79@gmail.com>
2024-08-08 19:12:35 -0400

    Worked on serving static content, and json files.

--- myApp.js
+++ myApp.js
@@ -1,8 +1,17 @@
 let express = require("express");
 let app = express();
 
+app.use("/public", express.static(__dirname + "/public"));
+// app.use(express.static("views/index.html"));
+
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
+  res.sendFile("views/index.html");
+});
+
+app.get("/json", (req, res) => {
+  res.json({
+    message: "Hello json",
+  });
 });
 
 module.exports = app;
