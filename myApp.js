d0af9ad2153fa60330c0f2b6e0bc4d81171fe067
Christopher Forti <christopher.forti.79@gmail.com>
2024-08-10 14:11:09 -0400

    wrote an if loop to manipulate json.

--- myApp.js
+++ myApp.js
@@ -4,14 +4,16 @@ require("dotenv").config();
 
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/index", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile("views/index.html");
 });
 
-app.get("/json", (req, res) => {
-  res.json({
-    message: "Hello json",
-  });
+app.get("/json", function (req, res) {
+  let message = "Hello json";
+  if (process.env.MESSAGE_STYLE === "uppercase") {
+    message = message.toUpperCase();
+  }
+  res.json({ message: message });
 });
 
 module.exports = app;
