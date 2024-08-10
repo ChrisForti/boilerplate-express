2a784891cda653e6dd1f690009f47664c83f6035
Christopher Forti <christopher.forti.79@gmail.com>
2024-08-10 14:56:41 -0400

    Started making notes for better understanding.

--- myApp.js
+++ myApp.js
@@ -2,16 +2,21 @@ let express = require("express");
 let app = express();
 require("dotenv").config();
 
+// calls in middleware
 app.use("/public", express.static(__dirname + "/public"));
+
+// logger to make requests
 app.use("/", (req, res, next) => {
   console.log(`${req.method} ${req.path} - ${req.ip}`);
   next();
 });
 
+// route that links root to index.html
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+// Uses .env and if loop to manipulate json
 app.get("/json", function (req, res) {
   let message = "Hello json";
   if (process.env.MESSAGE_STYLE === "uppercase") {
